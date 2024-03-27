df45a4bf03322067b5df57c1b4f428e4c231a1c3
wahhajb <164786203+wahhajb@users.noreply.github.com>
2024-03-28 00:03:26 +0300

    المطور.js

--- plugins/المطور.js
+++ plugins/المطور.js
@@ -2,7 +2,7 @@ function handler(m) {
   let data = global.owner.filter(([id, isCreator]) => id && isCreator)
   this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
 }
-handler.alias = ['المطور']
+handler.alias = ['https://wa.me/+967773289295']
 handler.command = /^(المالك|المطور)$/i
 
 export default handler
