d22d084a33e64e29d93706acae35ecd58abcb5fa
wahhajb <164786203+wahhajb@users.noreply.github.com>
2024-04-01 22:00:47 +0300

    Update بوت.js

--- plugins/بوت.js
+++ plugins/بوت.js
@@ -1,1 +1,15 @@
+let handler = async (m, { conn }) => {
+let done = '🩶'; 
+       m.react(done);
+    let user = global.db.data.users[m.sender];
+    let name = conn.getName(m.sender);
+    let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
+    let message = `هنا  تحط النص اللي بدك ياه`;
 
+    conn.sendFile(m.chat, 'تحط الصوره هنا تليجرام', 'image.jpg', message, m);
+};
+
+handler.customPrefix = /^(bot|بوت)$/i;
+handler.command = new RegExp;
+
+export default handler;
