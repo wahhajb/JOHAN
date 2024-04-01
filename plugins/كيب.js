8ed40ff0f0e0a275ed5cd70f1b16bc34dac70b58
wahhajb <164786203+wahhajb@users.noreply.github.com>
2024-04-01 22:56:47 +0300

    Update كيب.js

--- plugins/كيب.js
+++ plugins/كيب.js
@@ -1,1 +1,39 @@
+function handler(m, { text }) {
+  let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
+  m.reply(teks.replace(/[a-z]/gi, v => {
+      return { 
+          'a': '𝐴',
+          'b': '𝐵',
+          'c': '𝐶',
+          'd': '𝐷',
+          'e': '𝐸',
+          'f': '𝐹',
+          'g': '𝐺',
+          'h': '𝐻',
+          'i': '𝐼',
+          'j': '𝐽',
+          'k': '𝐾',
+          'l': '𝐿',
+          'm': '𝑀',
+          'n': '𝑁',
+          'o': '𝛩',
+          'p': '𝑃',
+          'q': '𝑄',
+          'r': '𝑅',
+          's': '𝑺',
+          't': '𝑇',
+          'u': '𝑈',
+          'v': '𝑉',
+          'w': '𝑊',
+          'x': '𝑋',
+          'y': '𝑌',
+          'z': '𝑍', 
+      }[v.toLowerCase()] || v
+  }))
+}
+handler.help = ['نسيانن١']
+handler.tags = ['نسيانن١']
+handler.command =  /^(كيب)$/i
 
+export default handler
+> `ᏫᏰᎥᎿᏫ`
