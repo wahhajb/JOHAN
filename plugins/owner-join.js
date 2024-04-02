ce7f982a3daa883ac7816bcef29e03e69ced3f91
wahhajb <164786203+wahhajb@users.noreply.github.com>
2024-04-02 05:38:17 +0300

    Update owner-join.js

--- plugins/owner-join.js
+++ plugins/owner-join.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, text, isMods, isOwner, isPrems }) => {
 let link = (m.quoted ? m.quoted.text ? m.quoted.text : text : text) || text
 let [_, code] = link.match(linkRegex) || []
 
-if (!code) throw '*[ ⚠️ خطأ ⚠️ ] الرابط غلط او مش موجود*\n*👉🏻 حط رابط الجروب*\n\n*مثال:*\n*.انضم https://chat.whatsapp.com/Bsoi5XfRcOiDTWSDOS2fLz*\n\n*[❗] لا ترد على أي رسالة قد تتسبب في حدوث تداخل ، اكتبها كرسالة جديدة فقط*'
+if (!code) throw '*[ ⚠️ خطأ ⚠️ ] الرابط غلط او مش موجود*\n*👉🏻 حط رابط الجروب*\n\n*مثال:*\n*.انضم https://chat.whatsapp.com/GlHhQPiYbF95GfsoecQByb*\n\n*[❗] لا ترد على أي رسالة قد تتسبب في حدوث تداخل ، اكتبها كرسالة جديدة فقط*'
 
 if ( isPrems || isMods || isOwner || m.fromMe) {
 let res = await conn.groupAcceptInvite(code)
