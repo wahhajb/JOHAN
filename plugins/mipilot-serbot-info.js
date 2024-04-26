52acb844f9a7049311061cadf7c2af17bb398042
jaljmay187 <144557543+jaljmay187@users.noreply.github.com>
2024-04-27 01:22:02 +0300

    Add files via upload

---
+++ plugins/mipilot-serbot-info.js
@@ -0,0 +1,7 @@
+async function handler(m, { usedPrefix }) {
+let users = [...new Set([...global.conns.filter(conn => conn.user && conn.state !== 'close').map(conn => conn.user)])]
+await m.reply( ' ')
+await m.reply(users.map(v => '👉🏻 wa.me/' + v.jid.replace(/[^0-9]/g, '') + `?text=${usedPrefix}estado (${v.name})`).join('\n'))}
+handler.command = handler.help = ['listjadibot','bots','subsbots']
+handler.tags = ['jadibot']
+export default handler
