3fd7fedb5f2396373c71ad5d0013f74ee02dff95
mikey3334 <168539891+mikey3334@users.noreply.github.com>
2024-04-30 15:06:12 +0300

    Update ronaldo.js

--- plugins/ronaldo.js
+++ plugins/ronaldo.js
@@ -6,5 +6,5 @@ conn.sendFile(m.chat, ronaldo, 'error.jpg', `*siuuuuuuuuuu*`, m)}
 //conn.sendButton(m.chat, "*Siiiuuuuuu*", author, ronaldo, [['⚽ NEXT ⚽', `${usedPrefix + command}`]], m)}
 handler.help = ['cristianoronaldo', 'cr7']
 handler.tags = ['img']
-handler.command = /^(رونالدو|cr7)$/i
+handler.command = /^(الدون|cr7)$/i
 export default handler
