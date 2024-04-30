f44f43ab460d0bf32806f539750b98eadd4e7b77
uxwgolo197 <167036124+uxwgolo197@users.noreply.github.com>
2024-04-30 03:08:34 +0300

    Update gpt-dx.js

--- plugins/gpt-dx.js
+++ plugins/gpt-dx.js
@@ -24,7 +24,7 @@ let handler = async (m, {
 
 handler.help = ["cleandx"];
 handler.tags = ["internet"];
-handler.command = /^(dx|bot|بوت|vcv)$/i;
+handler.command = /^(dx|bot|vcv)$/i;
 export default handler;
 
 async function CleanDx(your_qus) {
