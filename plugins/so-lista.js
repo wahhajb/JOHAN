68735706522979bc547e27b6a1985f43ba138605
mikey3334 <168539891+mikey3334@users.noreply.github.com>
2024-05-01 23:23:44 +0300

    Add files via upload

---
+++ plugins/so-lista.js
@@ -0,0 +1,200 @@
+let handler = m => m
+handler.all = async function (m) {
+let chat = global.db.data.chats[m.chat]
+global.db.data.users[m.sender].money += 50
+global.db.data.users[m.sender].exp += 50  
+    
+if (/^صوت انمي1|صوت-انمي1$/i.test(m.text) && chat.audios) {  
+if (!db.data.chats[m.chat].audios && m.isGroup) throw 0    
+let vn = './media/animevoice (1).mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}   
+
+if (chat.audios && m.text.match(/(صوت-انمي2|صوت انمي2)/gi)) {
+let vn = './media/animevoice (2).mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
+  
+if (chat.audios && m.text.match(/(صوت-انمي3|صوت انمي3)/gi)) {    
+let vn = './media/animevoice (3).mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
+    
+if (chat.audios && m.text.match(/(صوت-انمي4|صوت انمي4)/gi)) {    
+let vn = './media/animevoice (4).mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
+    
+if (chat.audios && m.text.match(/(صوت-انمي5|صوت انمي5)/gi)) {    
+let vn = './media/animevoice (5).mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
+    
+if (chat.audios && m.text.match(/(صوت-انمي6|صوت انمي6)/gi)) {    
+let vn = './media/animevoice (6).mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
+    
+if (chat.audios && m.text.match(/(صوت-انمي7|صوت انمي7)/gi)) {    
+let vn = './media/animevoice (7).mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
+    
+if (chat.audios && m.text.match(/(صوت-انمي8|صوت انمي8)/gi)) {    
+let vn = './media/animevoice (8).mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
+    
+if (chat.audios && m.text.match(/(صوت انمي9|صوت انمي9)/gi)) {    
+let vn = './media/animevoice (9).mp3 vives.mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
+    
+if (chat.audios && m.text.match(/(صوت-انمي10|صوت انمي10)/gi)) {    
+let vn = './media/animevoice (10).mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
+    
+if (chat.audios && m.text.match(/(صوت-انمي11|صوت انمي11)/gi)) {    
+let vn = './media/animevoice (11).mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
+    
+if (chat.audios && m.text.match(/(سوت-انمي12|صوت انمي12)/gi)) {    
+let vn = './media/animevoice (12).mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
+    
+if (chat.audios && m.text.match(/(صوت-انمي13|صوت انمي13)/gi)) {    
+let vn = './media/animevoice (13).mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
+    
+if (chat.audios && m.text.match(/(صوت-انمي14|صوت انمي14)/gi)) {    
+let vn = './media/animevoice (14).mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
+    
+if (chat.audios && m.text.match(/(صوت-انمي15|صوت انمي15)/gi)) {    
+let vn = './media/animevoice (15).mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
+    
+if (chat.audios && m.text.match(/(صوت-انمي16|صوت انمي16)/gi)) {    
+let vn = './media/animevoice (16).mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
+    
+if (chat.audios && m.text.match(/(صوت-انمي17|صوت انمي17)/gi)) {    
+let vn = './media/animevoice (17).mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
+    
+if (chat.audios && m.text.match(/(صوت-انمي18|صوت انمي18)/gi)) {    
+let vn = './media/animevoice (18).mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
+    
+if (chat.audios && m.text.match(/(الفاجر|المطرقع|المطرشم|القادر)/gi)) {    
+let vn = './media/voice.mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
+    
+if (chat.audios && m.text.match(/(الخول|العلق|عرص|كسمك|بيتناك)/gi)) {    
+let vn = './media/voice0.mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
+    
+if (chat.audios && m.text.match(/(صوت-انمي19|صوت انمي19)/gi)) {    
+let vn = './media/asen.mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
+    
+if (chat.audios && m.text.match(/(استغفرالله|استغفر الله)/gi)) {    
+let vn = './media/aa.mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
+    
+if (chat.audios && m.text.match(/(بوت علق|بوت خول|بوت تعبان|بوت عرص)/gi)) {    
+let vn = './media/Abot.mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
+    
+if (chat.audios && m.text.match(/(انتقال|اطير انا بقي)/gi)) {    
+let vn = './media/flash.mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
+    
+if (chat.audios && m.text.match(/(صوت-انمي20|صوت انمي20|موشي موش|موش موش)/gi)) {    
+let vn = './media/moshi moshi.mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
+    
+if (chat.audios && m.text.match(/(احلامي|احلامى)/gi)) {    
+let vn = './media/a7lame.mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
+    
+if (chat.audios && m.text.match(/(اوتاكو)/gi)) {    
+let vn = './media/otaku.mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
+    
+if (chat.audios && m.text.match(/(اوهايو)/gi)) {    
+let vn = './media/ohayo.mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
+    
+if (chat.audios && m.text.match(/(اوني-تشان|اوني تشان|صوت-انمي21)/gi)) {    
+let vn = './media/Onichan.mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
+    
+if (chat.audios && m.text.match(/(قحبة|قحبه)/gi)) {    
+let vn = './media/Yamete-kudasai.mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
+
+if (chat.audios && m.text.match(/(موجب|سالب)/gi)) {
+let vn = './media/انت موجب ولا سالب.ogg'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
+
+if (chat.audios && m.text.match(/(الدون|كريستيانو|رونالدو)/gi)) {    
+let vn = './media/siu.mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
+
+if (chat.audios && m.text.match(/(مشفر)/gi)) {    
+let vn = './media/Moshafer.mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
+
+if (chat.audios && m.text.match(/(القنصل)/gi)) {    
+let vn = './media/2اخويا القنصل.mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
+
+if (chat.audios && m.text.match(/(ايديت)/gi)) {    
+let vn = './media/menus/Menuvid1.mp4'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
+
+if (chat.audios && m.text.match(/(Shadow)/gi)) {    
+let vn = './media/menus/Menuvid2.mp4'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
+
+if (chat.audios && m.text.match(/(عمك-شادو)/gi)) {    
+let vn = './media/menus/Menuvid3.mp4'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
+  
+if (chat.audios && m.text.match(/(بوت متناك)/gi)) {    
+let vn = './media/بوت متناك.mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
+return !0 }
+export default handler
+function pickRandom(list) {
+return list[Math.floor(Math.random() * list.length)]}
