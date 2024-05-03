d966b1253f00ef0a452d968e3fc00748170f6657
mikey44d <168686508+mikey44d@users.noreply.github.com>
2024-05-03 13:04:52 +0300

    Update زوجني.js

--- plugins/زوجني.js
+++ plugins/زوجني.js
@@ -4,7 +4,7 @@ let res = await fetch('https://api.waifu.pics/sfw/waifu')
 if (!res.ok) throw await res.text()
 let json = await res.json()
 if (!json.url) throw `${lenguajeGB['smsAvisoFG']()}`
-conn.sendFile(m.chat, json.url, 'error.jpg', `  *زوجتك قمر زيك 😂✨*  `, m)
+conn.sendFile(m.chat, json.url, 'error.jpg', `  *مراتك بطل يسطا🫣*  `, m)
 conn.sendButton(m.chat, `😻😻😻😻`, wm, json.url, [['𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏 🆕', `/${command}`]], m)
 }
 handler.help = ['waifu']
