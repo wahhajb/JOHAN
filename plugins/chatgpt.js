07176c994123c6a755af790ad3f8ffcbc6965f38
mikey44d <168686508+mikey44d@users.noreply.github.com>
2024-05-04 23:03:01 +0300

    Update chatgpt.js

--- plugins/chatgpt.js
+++ plugins/chatgpt.js
@@ -3,7 +3,7 @@
 import fetch from 'node-fetch';
 const handler = async (m, { conn, text, usedPrefix, command }) => {
   if (!text) {
-    throw `قم بكتابة سؤالك\n\nمثال . .جرجيرر كيف حالك`;
+    throw `قم بكتابة سؤالك\n\nمثال . .ايانوكوجي كيف حالك`;
   }
   try {
     conn.sendPresenceUpdate('composing', m.chat);
