4e42a7a6255bee7b12b09349d7105eb69cce00e2
mikey44d <168686508+mikey44d@users.noreply.github.com>
2024-05-05 14:41:47 +0300

    Update fun-reto.js

--- plugins/fun-reto.js
+++ plugins/fun-reto.js
@@ -1,9 +1,9 @@
 let handler  = async (m, { conn }) => {
-conn.reply(m.chat,`*┌────「 سؤال 」─*\n*“${pickRandom(global.bucin)}”*\n*└────「 𝙕𝙊𝙍𝙊-𝘽𝙊𝙏 」─*`, m)
+conn.reply(m.chat,`*┌────「 تتوقع 」─*\n*“${pickRandom(global.bucin)}”*\n*└────「 𝐴𝑌𝐴𝑁𝛩𝐾𝛩𝑈𝐽𝐼-𝐵𝛩𝑇 」─*`, m)
 }
 handler.help = ['reto']
 handler.tags = ['fun']
-handler.command = /سؤال/i
+handler.command = /توقع/i
 export default handler
 
 function pickRandom(list) {
