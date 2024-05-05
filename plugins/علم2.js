067b917fba236ccfa201350f53d4dbf9884039b2
mikey44d <168686508+mikey44d@users.noreply.github.com>
2024-05-05 03:47:16 +0300

    Update علم2.js

--- plugins/علم2.js
+++ plugins/علم2.js
@@ -1,12 +1,31 @@
-let handler = async (m, { conn }) => {
+let timeout = 60000
+let poin = 500
+let handler = async (m, { conn, command, usedPrefix }) => {
     conn.tebakbendera = conn.tebakbendera ? conn.tebakbendera : {}
     let id = m.chat
-    if (!(id in conn.tebakbendera)) throw false
-    let json = conn.tebakbendera[id][1]
-    conn.reply(m.chat, '```' + json.name.replace(/[AIUEOaiueo]/ig, '_') + '```', m)
+    if (id in conn.tebakbendera) {
+        conn.reply(m.chat, '❐┃لم يتم الاجابة علي السؤال بعد┃❌ ❯', conn.tebakbendera[id][0])
+        throw false
+    }
+    let src = await (await fetch('https://raw.githubusercontent.com/mikey44d/MIKEYFOR-/d26bdb135cc33d1de9d9cd24eec34e90afcaf126/src/JSON/%D8%B9%D9%84%D9%85%20%20.json')).json()
+  let json = src[Math.floor(Math.random() * src.length)]
+    let caption = `*${command.toUpperCase()}*
+  ❐↞┇الـوقـت⏳↞ *${(timeout / 1000).toFixed(2)} ┇
+  *استخدم .انسحب للأنسحاب*
+  ❐↞┇الـجـائـزة💰↞ ${poin} نقاط┇
+*❰BY : 𝐴𝑌𝐴𝑁𝛩𝐾𝛩𝑈𝐽𝐼❱*
+     `.trim()
+    conn.tebakbendera[id] = [
+        await conn.sendFile(m.chat, json.question, '', caption, m),
+        json, poin,
+        setTimeout(() => {
+            if (conn.tebakbendera[id]) conn.reply(m.chat, `❮ ⌛┇انتهي الوقت┇⌛❯\n❐↞┇الاجـابـة✅↞ ${json.response}*┇`, conn.tebakbendera[id][0])
+            delete conn.tebakbendera[id]
+        }, timeout)
+    ]
 }
-handler.command = /اجابه$/i
-
-
+handler.help = ['guessflag']
+handler.tags = ['game']
+handler.command = /^علم/i
 
 export default handler
