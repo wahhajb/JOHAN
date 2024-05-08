74173909a3bb1a02e20d523cfcade42dea58fd8e
uxwgolo197 <167036124+uxwgolo197@users.noreply.github.com>
2024-05-09 02:25:41 +0300

    Update -focus.js

--- plugins/-focus.js
+++ plugins/-focus.js
@@ -1,25 +1,13 @@
-import fetch from  node-fetch ;
-const handler = async (m, { conn, text, usedPrefix, command }) => {
-  if (!text) {
-    throw `قم بكتابة سؤالك\n\nمثال . .جرجيرر كيف حالك`;
-  }
-  try {
-    conn.sendPresenceUpdate( composing , m.chat);
-    const BK9api = `https://api.bk9.site/ai/chatgpt?q=${encodeURIComponent(text)}`;
-    const BK99 = await fetch(BK9api);
-    const BK8 = await BK99.json();
-    if (BK8.status && BK8.BK9) {
-      const respuestaAPI = BK8.BK9;
-      conn.reply(m.chat, respuestaAPI, m);
-    } else {
-      throw "حدث خطأ أثناء معالجة طلبك.";
-    }
-  } catch (error) {
-    throw "حدث خطأ أثناء معالجة طلبك.";
-  }
-};
 
-handler.command = /^(1)$/i;
-handler.tags = [ ai ];
+let handler = async (m, { conn, text }) => {
+  let tagme = `https://wa.me/+${m.sender.replace(`+`)}/?text=حقوق+بوتك`
+  let mylink = [m.sender]
+  conn.reply(m.chat, tagme, m, { contextInfo: { mylink }})
+}
+handler.help = ['يرسل رابطك']
+handler.tags = ['group']
+handler.command = /^رابطي$/i
 
-export default handler;
+handler.group = false
+
+export default handler
