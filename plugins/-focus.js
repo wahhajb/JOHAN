f9d4162fe48e06edf2984866702472b37f85d11a
uxwgolo197 <167036124+uxwgolo197@users.noreply.github.com>
2024-05-09 18:31:42 +0300

    Update -focus.js

--- plugins/-focus.js
+++ plugins/-focus.js
@@ -1,13 +1,55 @@
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+    conn.relayMessage(m.chat, {
+      viewOnceMessage: {
+        message: {
+          interactiveMessage: {
+            header: {
+              title: '*قـائـمـة الـاوامــر*'
+            },
+            body: {
+              text: '🛡️ افتح القائمة بواسطة الزر\n⚡ لا تلعب كثير في القائمة'
+            },
+            nativeFlowMessage: {
+              buttons: [
+                {
+                  name: 'single_select',
+                  buttonParamsJson: JSON.stringify({
+                    title: 'دوس هنا ',
+                    sections: [
+                      {
+                        title: 'قوائم البوت',
+                        highlight_label: 'اختار',
+                        rows: [
+                          {
+                            header: 'قسم الانمي',
+                            title: '.الانمي',
+                            description: '',
+                            id: 'te'
+                          },
+                          {
+                            header: 'قسم الاوامر',
+                            title: '.المهام',
+                            description: '',
+                            id: 'te'
+                          }
+                        ]
+                      }
+                    ]
+                  }),
+                  messageParamsJson: ''
+                }
+              ]
+            }
+          }
+        }
+      }
+    }, {})
 
-let handler = async (m, { conn, text }) => {
-  let tagme = `https://wa.me/+${m.sender.replace(`+`)}/?text=حقوق+بوتك`
-  let mylink = [m.sender]
-  conn.reply(m.chat, tagme, m, { contextInfo: { mylink }})
 }
-handler.help = ['يرسل رابطك']
-handler.tags = ['group']
-handler.command = /^رابطي$/i
 
-handler.group = false
+handler.help = ['info']
+handler.tags = ['main']
+handler.command = ['قايمه']
 
 export default handler
+
