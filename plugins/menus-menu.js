b328e743c589d59b4e7f1e0a3cc75cfe1245f72d
mikey44d <168686508+mikey44d@users.noreply.github.com>
2024-05-14 23:34:22 +0300

    Update menus-menu.js

--- plugins/menus-menu.js
+++ plugins/menus-menu.js
@@ -19,7 +19,7 @@ let handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPr
     let uptime = clockString(_uptime)
 let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
-let videoUrl = 'https://telegra.ph/file/4b0037642a1ec84b39596.mp4';
+let videoUrl = 'https://telegra.ph/file/84e6871b389c3d4438332.mp4';
   let vn = './media/menu.mp3';
   const user = global.db.data.users[m.sender];
   const {money, joincount} = global.db.data.users[m.sender];
@@ -34,7 +34,7 @@ let rtotalreg = Object.values(global.db.data.users).filter(user => user.register
 let more = String.fromCharCode(8206)
 let readMore = more.repeat(900) 
   const taguser = '@' +  m.sender.split('@s.whatsapp.net')[0];
-let str = `𝐴𝑌𝐴𝑁𝛩𝐾𝛩𝑈𝐽𝐼-BOT 
+let str = `𝑺𝐻𝐴𝐷𝛩𝑊 𝐵𝛩𝑇
      
                    ‌
 *‏◆ ⋅ ┈──━ ⊰ ❊ ⊱ ━──┈ ⋅ ◆*
@@ -42,7 +42,7 @@ let str = `𝐴𝑌𝐴𝑁𝛩𝐾𝛩𝑈𝐽𝐼-BOT
 ⊑ ━═ 🃁⋖ قائـمـة 📰 الاوامـر ⋗🃁 ═━ ⊒
 ── • ◈ • ──
 ┇اهلا : *${taguser}*
-┇أنـا اسـمـــي "ايانوكوجي " 
+┇أنـا اسـمـــي "شادو " 
 ┇رقم المطـور : https://wa.me/201112422078
 ┇لا تنـسى تصلي على النـبي
 
