d632edc61190113344ebd3e790d0ace511fe1dd7
mikey44d <168686508+mikey44d@users.noreply.github.com>
2024-05-15 19:52:12 +0300

    Update اكس.js

--- plugins/اكس.js
+++ plugins/اكس.js
@@ -1,82 +1,54 @@
-import { TicTacToe } from '../lib/tictactoe.js'
-
+import TicTacToe from '../lib/tictactoe.js'
 let handler = async (m, { conn, usedPrefix, command, text }) => {
-    conn.game = conn.game ? conn.game : {}
-    if (text === 'حذففف') {
-        let room = Object.values(conn.game).find(room => room.id.startsWith('tictactoe') && [room.game.playerX, room.game.playerO].includes(m.sender))
-        if (room) {
-            delete conn.game[room.id];
-            m.reply('تم حذف اللعبة.')
-        } else {
-            m.reply('ليس لديك أي لعبة قائمة.')
-        }
-        return;
-    }
-    if (Object.values(conn.game).find(room => room.id.startsWith('tictactoe') && [room.game.playerX, room.game.playerO].includes(m.sender))) throw `❏ انت ماذلت في الجيم, لحذف الجيم اكتب : *${usedPrefix}حذففف*`
-    if (!text) throw `✳️ Put a number in the room`
-    let room = Object.values(conn.game).find(room => room.state === 'WAITING' && (text ? room.name === text : true))
-    if (room) {
-        m.reply('*تم ايجاد الشخص الاخر*')
-        room.o = m.chat
-        room.game.playerO = m.sender
-        room.state = 'PLAYING'
-        let arr = room.game.render().map(v => {
-            return {
-                X: '❎',
-                O: '⭕',
-                1: '1️⃣',
-                2: '2️⃣',
-                3: '3️⃣',
-                4: '4️⃣',
-                5: '5️⃣',
-                6: '6️⃣',
-                7: '7️⃣',
-                8: '8️⃣',
-                9: '9️⃣',
-            }[v]
-        })
-        let str = `
-انتظر @${room.game.currentTurn.split('@')[0]} هو اللاعب الاول
+conn.game = conn.game ? conn.game : {}
+if (Object.values(conn.game).find(room => room.id.startsWith('tictactoe') && [room.game.playerX, room.game.playerO].includes(m.sender))) throw '*[❗] انت مازلت في غرفه شخص ما*'
+if (!text) throw `*[❗] مطلوب وضع اسم لغرفه اللاعب*\n\n*—◉ مثال*\n*◉ ${usedPrefix + command} غرفه جديده*`
+let room = Object.values(conn.game).find(room => room.state === 'WAITING' && (text ? room.name === text : true))
+if (room) {
+await m.reply('*[🕹️] ابدأ اللعبه التي انضم اليها احد اللاعبين*')
+room.o = m.chat
+room.game.playerO = m.sender
+room.state = 'PLAYING'
+let arr = room.game.render().map(v => {
+return {
+X: '❎',
+O: '⭕',
+1: '1️⃣',
+2: '2️⃣',
+3: '3️⃣',
+4: '4️⃣',
+5: '5️⃣',
+6: '6️⃣',
+7: '7️⃣',
+8: '8️⃣',
+9: '9️⃣',
+}[v]})
+let str = `
+🎮 تيك تاك تو 🎮
 
-${arr.slice(0, 3).join('')}
-${arr.slice(3, 6).join('')}
-${arr.slice(6).join('')}
+❎ = @${room.game.playerX.split('@')[0]}
+⭕ = @${room.game.playerO.split('@')[0]}
 
-▢ *Room ID* ${room.id}
+        ${arr.slice(0, 3).join('')}
+        ${arr.slice(3, 6).join('')}
+        ${arr.slice(6).join('')}
 
-❏ *القوانين*
-‣ اصنع 3 صفوف من الرموز عموديا, أفقيا او انحرافي للفوز ‣ اكتب *surrender* للانسحاب
+دورك @${room.game.currentTurn.split('@')[0]}
 `.trim()
-        if (room.x !== room.o) await conn.reply(room.x, str, m, {
-            mentions: conn.parseMention(str)
-        })
-        await conn.reply(room.o, str, m, {
-            mentions: conn.parseMention(str)
-        })
-    } else {
-        room = {
-            id: 'tictactoe-' + (+new Date),
-            x: m.chat,
-            o: '',
-            game: new TicTacToe(m.sender, 'o'),
-            state: 'WAITING'
-        }
-        if (text) room.name = text
-
-        conn.reply(m.chat, `❏ *توقع الشريك*\nاكتب الامر التالي للدخول في نفس الجيم
-❏ *${usedPrefix + command} ${text}*
-
-❏ *الجائزه: 4999* اكس بي`, m, {
-            mentions: conn.parseMention(text)
-        })
-
-        conn.game[room.id] = room
-    }
-
-}
-
-handler.help = ['tictactoe <tag number>']
-handler.tags = ['game']
-handler.command = ['tictactoe', 'ttc', 'اكس', 'xo']
-
+if (room.x !== room.o) await conn.sendMessage(room.x, { text: str, mentions: this.parseMention(str)}, { quoted: m })
+await conn.sendMessage(room.o, { text: str, mentions: conn.parseMention(str)}, { quoted: m })
+} else {
+room = {
+id: 'tictactoe-' + (+new Date),
+x: m.chat,
+o: '',
+game: new TicTacToe(m.sender, 'o'),
+state: 'WAITING' }
+if (text) room.name = text     
+let imgplay = `https://cope-cdnmed.agilecontent.com/resources/jpg/8/9/1590140413198.jpg`
+conn.reply(m.chat, `*🕹 تيك تاك تو 🎮*\n\n*◉ في انتظار اللاعب الثاني*\n*◉ للخروج من اللعبه استخدم الأمر ${usedPrefix}delttt*\n\n◉ للانضمام الي نوع لعبه: (${usedPrefix + command} ${text})`, m)
+//conn.sendButton(m.chat, `*🕹 𝐓𝐑𝐄𝐒 𝐄𝐍 𝐑𝐀𝐘𝐀 🎮*\n\n*◉ 𝙴𝚂𝙿𝙴𝚁𝙰𝙽𝙳𝙾 𝙰𝙻 𝚂𝙴𝙶𝚄𝙽𝙳𝙾 𝙹𝚄𝙶𝙰𝙳𝙾𝚁*\n*◉ 𝙿𝙰𝚁𝙰 𝙱𝙾𝚁𝚁𝙰𝚁 𝙾 𝚂𝙰𝙻𝙸𝚁𝚂𝙴 𝙳𝙴 𝙻𝙰 𝙿𝙰𝚁𝚃𝙸𝙳𝙰 𝚄𝚂𝙴𝙽 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 ${usedPrefix}delttt*`, wm, imgplay, [['𝚄𝙽𝙸𝚁𝚂𝙴 𝙰 𝙻𝙰 𝙿𝙰𝚁𝚃𝙸𝙳𝙰', `${usedPrefix + command} ${text}`]], m, { mentions: conn.parseMention(text) })
+conn.game[room.id] = room
+}}
+handler.command = /^(tictactoe|لعبه|ttc|ttt|اكس|xo)$/i
 export default handler
