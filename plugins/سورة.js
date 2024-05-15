a47dc6ad275cce7b44a89f426d8b96cc3fc35460
mikey44d <168686508+mikey44d@users.noreply.github.com>
2024-05-15 15:06:44 +0300

    Update سورة.js

--- plugins/سورة.js
+++ plugins/سورة.js
@@ -1,19 +1,20 @@
-import {alquran} from '@bochilteam/scraper'
+import { alquran } from '@bochilteam/scraper'
+
 let handler = async (m, { args, usedPrefix, command }) => {
-    if (!(args[0] || args[1])) throw `البحث عن أي آية في القرآن بالصوت والقراءة :\n${usedPrefix + command} 1 2\n\nاذا لم تفهم اي شيء راسلني  هنا \n https://wa.me/+201112422078`
-    if (isNaN(args[0]) || isNaN(args[1])) throw `contoh:\n${usedPrefix + command} 1 2\n\nhttps://wa.me/+201112422078`
+    if (!(args[0] || args[1])) throw `✳️ البحث عن أي آية في القرآن بالصوت والقراءة:\n${usedPrefix + command} 1 2\n\n يقوم الامر بختصار على البحث عن الاية التي تكتب رقمها أو يرسلها مع مقطع صوتي يقرأ الآية `
+    if (isNaN(args[0]) || isNaN(args[1])) throw `مثال:\n${usedPrefix + command} 1 2\n\n𝙱𝚢 : 𝚑𝚊𝚝𝚖𝚒𝚔𝚞 - 𝚋𝚘𝚝`
     let api = await alquran()
     let mes = `
 ${api[args[0] - 1].ayahs[args[1] - 1].text.ar}
-
+    
 ${api[args[0] - 1].ayahs[args[1] - 1].translation.en}
-( Q.S ${api[args[0] - 1 ].asma.en.short} : ${api[args[0] - 1].ayahs[args[1] - 1].number.insurah} )
+( Q.S ${api[args[0] - 1].asma.en.short} : ${api[args[0] - 1].ayahs[args[1] - 1].number.insurah} )
 `.trim()
     m.reply(mes)
     conn.sendFile(m.chat, api[args[0] - 1].ayahs[args[1] - 1].audio.url, '', '', m)
 }
 
-handler.help = ['ayta'].map(v => v + ' *surah no*')
+handler.help = ['ayta'].map(v => v + ' *surah*')
 handler.tags = ['islam']
-handler.command = /^(سوره|سورة)$/i
+handler.command = /^(ايه|ایه|آيه|أية)$/i
 export default handler
