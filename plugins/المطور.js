9a27da734a65d90c67470eaba460c94d339527bd
mikey44d <168686508+mikey44d@users.noreply.github.com>
2024-05-17 20:23:13 +0300

    Update المطور.js

--- plugins/المطور.js
+++ plugins/المطور.js
@@ -19,7 +19,7 @@ let handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPr
     let uptime = clockString(_uptime)
 let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
-let videoUrl = 'https://telegra.ph/file/1a05a39d4a949fbf32e7a.mp4';
+let videoUrl = 'https://telegra.ph/file/bd396a5a46620ac745a37.mp4';
   let vn = './media/menu.mp3';
   const user = global.db.data.users[m.sender];
   const {money, joincount} = global.db.data.users[m.sender];
