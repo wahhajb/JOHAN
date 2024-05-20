10bba9883207d47a34f438bbc99d9361d5fd4ed3
mikey44d <168686508+mikey44d@users.noreply.github.com>
2024-05-20 11:10:04 +0300

    Update مساقبة  .js

--- plugins/مساقبة  .js
+++ plugins/مساقبة  .js
@@ -1,114 +1,147 @@
-import fetch from "node-fetch";
+جلب الاستيراد من "node-fetch"؛
 
-let points = 50;
-let maxPlayers = 10;
-let maxQuestions = 50;
-let questionTimeout = 25 * 1000; // 25 ثانية
+دع النقاط = 50؛
+دع maxPlayers = 10;
+دع maxQuestions = 50؛
+دع questionTimeout = 25 * 1000؛ // 25 ثانيه لو عايز تغيرها براحتك
 
-let handler = async (m, { conn, command }) => {
-    let id = m.chat;
-    conn.mikey = conn.mikey ? conn.mikey : {};
+دع المعالج = غير متزامن (م، {كون، الأمر }) => {
+    اسمحوا معرف = m.chat;
+    conn.venom3mk = conn.venom3mk ? conn.venom3mk : {};
 
-    if (command === "مسابقه-صور") {
-        if (id in conn.mikey) {
-            conn.reply(m.chat, '*المسابقة مستمرة بالفعل!*', conn.mikey[id][0]);
-            throw false;
+    إذا (الأمر === "مسابقه-صور") {
+        إذا (المعرف في conn.venom3mk) {
+            conn.reply(m.chat, '*المسابقه المستمر في اللعب وفوز*', conn.venom3mk[id][0]);
+            رمي كاذبة.
         }
 
-        conn.mikey[id] = [
-            await conn.reply(m.chat, '1 - جاوب على السؤال بسرعة\n2 - اجمع 50 نقطة\n3 - اتبع التعليمات', m), [], [], 0, 0, null
+        conn.venom3mk[المعرف] = [
+            انتظار conn.reply(m.chat, '1 - جاوب علي السوأل بسرعه\n2 - جمع 50 نقطه\n3 - اتبع التعليمات', m), [], [], 0, 0, null
         ];
 
-        conn.reply(m.chat, '🎡| بدأت المسابقة، اكتب " #انضم-صور" للانضمام', m);
-        throw false;
+        conn.reply(m.chat, '🎡| بدأت أوت اكتب " #انضم-صور" لانضمام الاعبين ', m);
+        رمي كاذبة.
     } else if (command === "انضم-صور") {
-        if (!(id in conn.mikey)) {
-            conn.reply(m.chat, 'لا توجد مسابقة جارية حالياً!', m);
-            throw false;
+        إذا (!(المعرف في conn.venom3mk)) {
+            conn.reply(m.chat, 'لا يوجد مسابقة قائمة حاليا!', m);
+            رمي كاذبة.
         }
 
-        if (conn.mikey[id][2].length >= maxPlayers) {
+        إذا (conn.venom3mk[id][2].length >= maxPlayers) {
             conn.reply(m.chat, 'اكتمل العدد', m);
-            throw false;
+            رمي كاذبة.
         }
 
-        if (conn.mikey[id][2].findIndex(player => player.id === m.sender) !== -1) {
+        إذا (conn.venom3mk[id][2].findIndex(player => player.id === m.sender) !== -1) {
             conn.reply(m.chat, 'أنت مسجل بالفعل', m);
-            throw false;
+            رمي كاذبة.
         }
 
-        conn.mikey[id][2].push({ id: m.sender, points: 0, correctAnswers: 0 });
-        conn.reply(m.chat, `تم التسجيل بنجاح! العدد الكامل والمشاركة: ${maxPlayers - conn.mikey[id][2].length}`, m);
+        conn.venom3mk[id][2].push({ id: m.sender, points: 0, CorrectAnswers: 0 });
+        conn.reply(m.chat, `تم التسجيل الفعال! العدد الكامل والمشاركة: ${maxPlayers - conn.venom3mk[id][2].length}`, m);
 
-        if (conn.mikey[id][2].length >= 2) {
-            let mikeyData = await (await fetch(`https://raw.githubusercontent.com/DK3MK/worker-bot/main/eye.json`)).json();
-            let json = mikeyData[Math.floor(Math.random() * mikeyData.length)];
-            conn.mikey[id][1] = json;
-            let playersList = conn.mikey[id][2].map((player, i) => `${i + 1} - @${player.id.split('@')[0]} [${ player.points} نقطة]`).join('\n');
-            let caption = `━ ───── • ⟐ • ───── ━
-*• السؤال رقم ${conn.mikey[id][4] + 1}*
-*• جاوب بسرعة*
-*• الجائزة:* ⌊ ${points} ⌉ *نقطة* لكل إجابة صحيحة
-━ ───── • ⟐ • ───── ━`.trim();
-            conn.sendFile(m.chat, json.img, '', caption, m);
+        إذا (conn.venom3mk[id][2].length >= 2) {
+            دع venom3mk = ينتظر (ينتظر الجلب(`https://raw.githubusercontent.com/DK3MK/worker-bot/main/eye.json`)).json();
+            Let json = venom3mk[Math.floor(Math.random() * venom3mk.length)];
+            conn.venom3mk[id][1] = json;
+            دع اللاعبينList = conn.venom3mk[id][2].map((player, i) => `${i + 1} - @${player.id.split('@')[0]} [${ player.points} نقطة]`).join('\n');
+            اسمحوا التسمية التوضيحية = `━ ───── • ⟐ • ───── ━
+*• السؤال رقم ${conn.venom3mk[id][4] + 1}*
+*• جاوب بسرعه بحلو*
+*• الجائزة:* ⌊ ${points} ⌉ *نقطة* , \`لكل إجابه صحيحه\`
+━ ───── • ⟐ • ───── ━`.trim()//\n\n${playersList}
+            conn.sendFile(m.chat, json.img, '', تسمية توضيحية, m)
 
-            conn.mikey[id][5] = setTimeout(() => {
-                let correctIndex = conn.mikey[id][2].findIndex(player => player.id === m.sender);
-                if (correctIndex !== -1) {
-                    conn.mikey[id][2][correctIndex].points += points;
-                    conn.mikey[id][2][correctIndex].correctAnswers++;
-                    conn.reply(m.chat, `انتهى الوقت! الإجابة الصحيحة هي: ${json.name}\n+${points} نقطة`, conn.mikey[id][0]);
-                } else {
-                    conn.reply(m.chat, `انتهى الوقت! الإجابة الصحيحة هي: ${json.name}`, conn.mikey[id][0]);
-                }
-
-                clearTimeout(conn.mikey[id][5]);
-                conn.mikey[id][5] = null;
+            conn.venom3mk[id][5] = setTimeout(() => {
+                conn.reply(m.chat, `الوقت انتهى! الإجابة الصحيحة هي: ${json.name}`, conn.venom3mk[id][0]);
+                ClearTimeout(conn.venom3mk[id][5]);
+                conn.venom3mk[id][5] = null;
 
                 setTimeout(async () => {
-                    let newJson = mikeyData[Math.floor(Math.random() * mikeyData.length)];
-                    conn.mikey[id][1] = newJson;
-                    conn.mikey[id][3]++;
-                    conn.mikey[id][4]++;
+                    Let newJson = venom3mk[Math.floor(Math.random() * venom3mk.length)];
+                    conn.venom3mk[id][1] = newJson;
+                    conn.venom3mk[id][3]++;
+                    conn.venom3mk[id][4]++;
 
-                    let newCaption = `━ ───── • ⟐ • ───── ━
-*• السؤال رقم ${conn.mikey[id][4] + 1}*
-*• جاوب بسرعة*
-*• الجائزة:* ⌊ ${points} ⌉ *نقطة* لكل إجابة صحيحة
-━ ───── • ⟐ • ───── ━`.trim();
-                    conn.sendFile(m.chat, newJson.img, '', newCaption, m);
+                    دع newCaption = `━ ───── • ⟐ • ───── ━
+*• السؤال رقم ${conn.venom3mk[id][4] + 1}*
+*• جاوب بسرعه بحلو*
+*• الجائزة:* ⌊ ${points} ⌉ *نقطة* , \`لكل إجابه صحيحه\`
+━ ───── • ⟐ • ───── ━`.trim()//\n\n${playersList}
+                    conn.sendFile(m.chat, newJson.img, '', newCaption, m)
                 }, 1000); // تأخير إرسال سؤال جديد لتصور أفضل
             }, questionTimeout);
         }
     } else if (command === "حذف-صور") {
-        if (!conn.mikey[id]) {
-            conn.reply(m.chat, 'لم تبدأ اللعبة بعد', m);
-        } else {
-            clearTimeout(conn.mikey[id][5]); // مسح المهلة إن وجدت
-            delete conn.mikey[id];
-            conn.reply(m.chat, 'تم حذف اللعبة بنجاح', m);
+        إذا (!conn.venom3mk[id]) {
+            conn.reply(m.chat, 'لـم تـبـدأ الـمـبـاره بـعـد', m);
+        } آخر {
+            ClearTimeout(conn.venom3mk[id][5]); // مسح المهلة إن وجدت
+            حذف conn.venom3mk[id];
+            conn.reply(m.chat, 'تـم حـذف الـلـعـبـه بـنـجـاح', m);
         }
     }
 };
 
-handler.before = async function (m, { conn }) {
-    let id = m.chat;
-    this.mikey = this.mikey ? this.mikey : {};
+Handler.before = وظيفة غير متزامنة (m, { conn }) {
+    اسمحوا معرف = m.chat;
+    this.venom3mk = this.venom3mk ؟ this.venom3mk : {};
 
-    if (!(id in this.mikey)) return;
+    if (!(id in this.venom3mk)) return;
 
-    let json = this.mikey[id][1];
-    let players = this.mikey[id][2];
-    let questionCount = this.mikey[id][3];
+    Let json = this.venom3mk[id][1];
+    دع اللاعبين = this.venom3mk[id][2];
+    Let questionCount = this.venom3mk[id][3];
 
-    if (json && json.name && m.text.toLowerCase() === json.name.toLowerCase()) {
-        clearTimeout(this.mikey[id][5]); // مسح المهلة
-        let playerIndex = players.findIndex(player => player.id === m.sender);
-        if (playerIndex !== -1) {
-            players[playerIndex].points += points;
-            players[playerIndex].correctAnswers++;
-            questionCount++;
+    إذا (json && json.name && m.text.toLowerCase() === json.name.toLowerCase()) {
+        ClearTimeout(this.venom3mk[id][5]); // مسح المهلة
+        Let playerIndex = player.findIndex(player => player.id === m.sender);
+        اللاعبين[playerIndex].points += نقاط;
+        اللاعبين[playerIndex].correctAnswers++;
+        questionCount++;
 
-            if (questionCount >= maxQuestions) {
-                let sortedPlayers = players.sort((a, b) => b.points - a.points);
-                let playersList = sortedPlayers.map((player, i) => `${i + 1} - @${
+        إذا (questionCount >= maxQuestions) {
+            LetsortedPlayers = player.sort((a, b) => b.points - a.points);
+            Let playerList =sortedPlayers.map((player, i) => `${i + 1} - @${player.id.split('@')[0]} [${player.points} نقطة, ${ player.correctAnswers} إعلانات صحيحة]`).join('\n');
+            this.reply(m.chat, `المسابقة! هنا النتائج:\n\n${playersList}`, m, { الإشارات: conn.parseMention(playersList) });
+            احذف this.venom3mk[id];
+        } آخر {
+            دع venom3mk = ينتظر (ينتظر الجلب(`https://raw.githubusercontent.com/DK3MK/worker-bot/main/eye.json`)).json();
+            json = venom3mk[Math.floor(Math.random() * venom3mk.length)];
+            this.venom3mk[id][1] = json;
+            this.venom3mk[id][3] = questionCount;
+            this.venom3mk[id][4]++;
+            السماح للاعبينList = اللاعبين.خريطة((player, i) => `${i + 1} - @${player.id.split('@')[0]} [${player.points} نقطة, ${ player.correctAnswers} إعلانات صحيحة]`).join('\n');
+            اسمحوا التسمية التوضيحية = `━ ───── • ⟐ • ───── ━
+*• السؤال رقم ${this.venom3mk[id][4] + 1}*
+*• جاوب بسرعه بحلو*
+*• الجائزة:* ⌊ ${points} ⌉ *نقطة* , \`لكل إجابه صحيحه\`
+━ ───── • ⟐ • ───── ━`.trim()//\n\n${playersList}
+            this.sendFile(m.chat, json.img, '', تسمية توضيحية, m)
+
+ 
+            this.venom3mk[id][5] = setTimeout(() => {
+                this.reply(m.chat, `الوقت انتهى! الإجابة الصحيحة هي: ${json.name}`, this.venom3mk[id][0]);
+                ClearTimeout(this.venom3mk[id][5]);
+                this.venom3mk[id][5] = null;
+
+                setTimeout(async () => {
+                    Let newJson = venom3mk[Math.floor(Math.random() * venom3mk.length)];
+                    this.venom3mk[id][1] = newJson;
+                    this.venom3mk[id][3]++;
+                    this.venom3mk[id][4]++;
+
+                    دع newCaption = `━ ───── • ⟐ • ───── ━
+*• السؤال رقم ${this.venom3mk[id][4] + 1}*
+*• جاوب بسرعه بحلو*
+*• الجائزة:* ⌊ ${points} ⌉ *نقطة* , \`لكل إجابه صحيحه\`
+━ ───── • ⟐ • ───── ━`.trim()//\n\n${playersList}
+                    this.sendFile(m.chat, newJson.img, '', newCaption, m)
+                }, 1000); // تأخير إرسال سؤال جديد لتصور أفضل
+            }, questionTimeout);
+        }
+    }
+};
+Handler.command = /^(مسابقه-صور|انضم-صور|حذف-صور)$/i;
+
+معالج التصدير الافتراضي؛
