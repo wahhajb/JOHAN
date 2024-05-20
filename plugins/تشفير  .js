6187d1d40c3055cfb3c3aad1bff1cfdeff6df179
mikey44d <168686508+mikey44d@users.noreply.github.com>
2024-05-20 20:30:01 +0300

    Update تشفير  .js

--- plugins/تشفير  .js
+++ plugins/تشفير  .js
@@ -1,71 +1,73 @@
-استيراد js من "javascript-obfuscator"؛
+import JavaScriptObfuscator from 'javascript-obfuscator';
 
-دع المعالج = غير متزامن ( m, { conn, text } ) => {
-إذا (!m.quoted.text) قم بإلقاء "قم بالرد على الكود الذي تريد ترجمته!"؛
+let handler = async (m, { conn, text }) => {
+    if (!m.quoted.text) throw "قم بالرد على الكود الذي تريد ترجمته!";
+    
+    let obfuscationResult = JavaScriptObfuscator.obfuscate(m.quoted.text, {
+        compact: true,
+        controlFlowFlattening: false,
+        controlFlowFlatteningThreshold: 0.75,
+        deadCodeInjection: false,
+        deadCodeInjectionThreshold: 0.4,
+        debugProtection: false,
+        debugProtectionInterval: 0,
+        disableConsoleOutput: false,
+        domainLock: [],
+        domainLockRedirectUrl: 'about:blank',
+        forceTransformStrings: [],
+        identifierNamesCache: null,
+        identifierNamesGenerator: 'hexadecimal',
+        identifiersDictionary: [],
+        identifiersPrefix: '',
+        ignoreRequireImports: false,
+        inputFileName: '',
+        log: false,
+        numbersToExpressions: false,
+        optionsPreset: 'default',
+        renameGlobals: false,
+        renameProperties: false,
+        renamePropertiesMode: 'safe',
+        reservedNames: [],
+        reservedStrings: [],
+        seed: 0,
+        selfDefending: false,
+        simplify: true,
+        sourceMap: false,
+        sourceMapBaseUrl: '',
+        sourceMapFileName: '',
+        sourceMapMode: 'separate',
+        sourceMapSourcesMode: 'sources-content',
+        splitStrings: false,
+        splitStringsChunkLength: 10,
+        stringArray: true,
+        stringArrayCallsTransform: true,
+        stringArrayCallsTransformThreshold: 0.5,
+        stringArrayEncoding: [],
+        stringArrayEncodingType: [
+            'hexadecimal'
+        ],
+        stringArrayIndexShift: true,
+        stringArrayRotate: true,
+        stringArrayShuffle: true,
+        stringArrayWrappersCount: 1,
+        stringArrayWrappersChainedCalls: true,
+        stringArrayWrappersParametersMaxCount: 2,
+        stringArrayWrappersType: 'variable',
+        stringArrayThreshold: 0.75,
+        target: 'browser',
+        transformObjectKeys: false,
+        unicodeEscapeSequence: false
+    });
 
-دع الدقة = js.obfuscate( m.quoted.text, {
-    المدمجة: صحيح،
-    التحكم في التدفق: خطأ،
-    التحكم في التدفق والتسطيح: 0.75،
-    حقن الكود الميت: خطأ،
-    deadCodeInjectionThreshold: 0.4,
-    حماية التصحيح: خطأ،
-    تصحيح الأخطاء: 0،
-    DisableConsoleOutput: خطأ،
-    قفل المجال: []،
-    domainLockRedirectUrl: 'حول: فارغ'،
-    سلاسل تحويل القوة: []،
-    معرف أسماء ذاكرة التخزين المؤقت: فارغة،
-    معرف أسماء المولدات: 'سداسي عشري'،
-    معرفات القاموس: []،
-    بادئة المعرفات: ''،
-    تجاهلRequireImports: خطأ،
-    اسم ملف الإدخال: ''،
-    سجل: كاذبة،
-    أرقام إلى التعبيرات: خطأ،
-    الخيارات المسبقة: "افتراضي"،
-    إعادة تسمية Globals: خطأ،
-    إعادة تسمية الخصائص: خطأ،
-    إعادة تسمية خصائص الوضع: "آمن"،
-    الأسماء المحجوزة: []،
-    سلاسل محجوزة: []،
-    البذور: 0،
-    الدفاع عن النفس: كاذب،
-    تبسيط: صحيح،
-    خريطة المصدر: خطأ،
-    SourceMapBaseUrl: ''،
-    اسم ملف المصدر: ''،
-    sourceMapMode: "منفصل"،
-    sourceMapSourcesMode: "مصادر المحتوى"،
-    سلاسل الانقسام: كاذبة،
-    SplitStringsChunkLength: 10،
-    سلسلة الصفيف: صحيح،
-    سلسلة ArrayCallsTransform: صحيح،
-    stringArrayCallsTransformThreshold: 0.5،
-    ترميز الصفيف: []،
-    نوع سلسلة الصفيف: [
-        "رقم سداسي عشري"
-    ]،
-    stringArrayIndexShift: صحيح،
-    سلسلة ArrayRotate: صحيح،
-    stringArrayShuffle: صحيح،
-    stringArrayWrappersCount: 1،
-    stringArrayWrappersChainedCalls: صحيح،
-    stringArrayWrappersParametersMaxCount: 2,
-    stringArrayWrappersType: "متغير"،
-    stringArrayThreshold: 0.75،
-    الهدف: "المتصفح"،
-    تحويل مفاتيح الكائن: خطأ،
-    unicodeEscapeSequence: خطأ
-}).getObfuscatedCode()
-  
- إذا (!res) رمي "خطأ :("
-إرجاع m.reply(res);
+    let result = obfuscationResult.getObfuscatedCode();
+    
+    if (!result) throw "خطأ :(";
+    return m.reply(result);
 }
 
-Handler.help = ['enc']
-Handler.tags = ['أدوات']
-Handler.alias = ['enc']
-Handler.command = /^(enc|تشفير)$/i
+handler.help = ['enc']
+handler.tags = ['tools']
+handler.alias = ['enc']
+handler.command = /^(enc|تشفير)$/i
 
-معالج التصدير الافتراضي؛
+export default handler;
