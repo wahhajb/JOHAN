61f47b9e9bd289e3eebb854ebaf4f1c808919f12
mikey44d <168686508+mikey44d@users.noreply.github.com>
2024-05-23 21:13:51 +0300

    Add files via upload

---
+++ plugins/owner-unbanuser.js
@@ -0,0 +1,10 @@
+let handler = async (m) => {
+global.db.data.chats[m.chat].isBanned = false
+m.reply('* تم إلغاء حظر هذه الدردشة بنجاح*')
+}
+handler.help = ['unbanchat']
+handler.tags = ['owner']
+handler.command = /^الغاء-الحظر|رفع-الحظر$/i
+handler.rowner = true
+export default handler
+
